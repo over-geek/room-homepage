b27c93df2ab567e2f3ecb2e67f2530eb1432c3cc
over-geek <kenagy100@gmail.com>
2024-06-12 04:57:51 +0000

    Add header and mobile menu components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import MobileMenu from "./MobileMenu";
+
+const Header = () => {
+  return (
+    <>
+      <MobileMenu />
+      <div className="header"></div>
+    </>
+  );
+}
+
+export default Header;
