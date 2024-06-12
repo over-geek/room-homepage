f42371c8f63c3042580612dab5d9d3e628e0444f
over-geek <kenagy100@gmail.com>
2024-06-12 23:48:00 +0000

    Import About component into main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
 
   return (
     <div>
       <Header />
+      <About />
     </div>
   )
 }
