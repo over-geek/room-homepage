dd57c3c08c0cd62824a07bd6b856714eeefff13b
over-geek <kenagy100@gmail.com>
2024-06-19 19:03:29 +0000

    Modify breakpoints

---
+++ src/utils/theme.tsx
@@ -0,0 +1,9 @@
+export default {
+  mediaQueries: {
+    desktop: '@media (min-width: 901px)',
+    tablet: '@media (min-width: 500px) and (max-width: 900px)',
+    mobile: '@media (max-width: 499px)',
+    mobileSmall : '@media (max-width: 375px)',
+    menuBreakpoint: '@media (min-width: 500px)',
+  }
+}
